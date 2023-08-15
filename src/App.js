948fd1bbe47c9108c40244a3677a7f9ccce4c18b
seva2305 <sevabhalodiya2305@gmail.com>
2023-08-15 17:25:09 +0530

    new components added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Form from "./components/form";
 
 function App() {
   return (
     <div className="App">
-<form>
-  <lable>
-    name:
-    <input type="text" name="name" />
-    </lable>
-    <input type="submit" value="submit" />
-</form>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      {/* <h1> hello</h1>
-      <h2> every</h2>
-      <h3> one</h3>
-      <h3> one</h3>
-      <h2> every</h2>
-      <h1> hello</h1> */}
+
+      <Form/>
     </div>
   );
 }
---
+++ src/components/form.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {useState} from "react";
+const Form = () => {
+    const [data,setData] = useState(0);
+       const Clicked = () => {
+        setData(data +1);
+       }
+       const Reset = () => {
+        setData(0);
+       }
+
+
+    return (
+        <>
+            <form>
+                <lable>
+                    name:
+                    <input type="text" name="name" />
+                </lable>
+                <input type="submit" value="submit" />
+            </form>
+            <h1> button click {data} add</h1>
+            <button onClick={Clicked}>click</button>
+            <button onClick={Reset} >reset</button>
+        
+        </>
+    );
+
+}
+export default Form;
