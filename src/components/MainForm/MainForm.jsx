82ddfb4b37fbce774e821b94cd0b1682c95da275
seva2305 <sevabhalodiya2305@gmail.com>
2023-08-19 23:52:02 +0530

    form unscessesfully

--- src/components/MainForm/MainForm.jsx
+++ src/components/MainForm/MainForm.jsx
@@ -14,12 +14,9 @@ const MainForm = () => {
     const [age, setAge] =useState('');
     const [ageErr, setAgeErr] =useState(false);
  
-
-
-
     const fnameHandle = (e) => {
         let item =e.target.value;
-        if (item.length >=3 && item.length <=7) {
+        if (item.length >=3 && item.length <=7 || item.length==0) {
             setFnameErr(false)
         }
         else {
@@ -27,49 +24,76 @@ const MainForm = () => {
         }
         setFname(item)
     }
-    const valueHandle = () => {
-        if (fnameErr == true)
-        {
-            alert('your First name is Invalid');
+      const fnamevaluehandle = (e) => {
+        e.preventDefault();
+        if (fnameErr == true || fname.length==0){
+        alert("first name is Invalid")
         }
-    }
+        if (lnameErr == true || lname.length==0){
+            alert("last name is Invalid")
+            }
+            if (emailErr == true || email.length==0){
+                alert("email name is Invalid")
+                }
+                if (phoneErr == true || phone.length==0){
+                    alert("phone name is Invalid")
+                    }
+                    if (ageErr == true || age.length==0){
+                        alert("age name is Invalid")
+                        }
+      } 
+
     const lnameHandle = (e) => {
-        let lnameitem =e.target.value;
-        if (lnameitem.length >=5 && lnameitem.length <=9) {
+        let item =e.target.value;
+        if (item.length >=5 && item.length <=9 || item.length==0 ) {
             setLnameErr(false)
         }
         else {
             setLnameErr(true)
         }
-        setLast(lnameitem);
+        setLast(item);
     }
     const valuesHandle = () => {
         if (lnameErr == true);
         {
             alert('your Last name is Invalid');
         }
+        
     }
     const emailHandle = (e) => {
-         let regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+         let regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g 
          let emailvalue = e.target.value;
          setEmailErr(!(regex.test(emailvalue)));
          setEmail(emailvalue); 
+
+         if(emailvalue.length==0){
+            setEmailErr (false)
+         }
+         
     }
     const phoneHandle = (e) => {
         let regex =/^[6-9]\d{9}$/g
         let phonevalue = e.target.value;
         setPhoneErr(!(regex.test(phonevalue)));
-        setPhone(phonevalue);   
+        setPhone(phonevalue);  
+        
+        if (phonevalue.length==0){
+            setPhoneErr (false)
+        }
+        
     }
     const ageHandle = (e) => {
         let ageitem =e.target.value;
         console.log ("this is" ,age);
-        if (ageitem >16 && ageitem < 100 ){
+        if (ageitem >= 18 && ageitem <= 100 ){
         setAgeErr (false);
     }
     else {
         setAgeErr (true);
     }
+       if (ageitem.length==0){
+           setAgeErr(false)
+       }
     setAge(ageitem)
 
     }
@@ -79,7 +103,7 @@ const MainForm = () => {
             <div className="mainContainer">
                 <div className="mainForm">
                     <h1 className='mainHeading'>Regestration Form</h1>
-                    <form className='formBox' onSubmit={valuesHandle} >
+                    <form className='formBox' onSubmit={ fnamevaluehandle} >
                         <label className='mainLabel'>First Name:</label>
                         <input type="text" name="firstname" onChange={fnameHandle} className='mainInput' />{fnameErr?<span className='ErroeMessage'>First Name Invalid</span>:""} <br/>
                         <label className='mainLabel'>Last Name:</label>
@@ -89,11 +113,11 @@ const MainForm = () => {
                         <input type="text" name="email" onChange={emailHandle} className='mainInput' /> {emailErr?<span className='ErroeMessage'> Email is Invalid</span>:""}
                         <br/>
                          <label className='mainLabel'>Phone:</label>
-                        <input type="text" name="phone" onChange={phoneHandle} className='mainInput' /> {phoneErr?<span className='ErrorMessage'>Phone no. is Invalid</span>:""}
+                        <input type="text" name="phone" onChange={phoneHandle} className='mainInput' /> {phoneErr?<span className='ErroeMessage'>Phone no. is Invalid</span>:""}
                         <br/>
                         <label className='mainLabel'>Age:</label>
-                        <input type="text" name="age" onChange={ageHandle} className='mainInput' />{ageErr?<span className='ErrorMessage'>Age is Invalid</span>:""}
-                        <button type="button" className='mainButton'>submit</button>
+                        <input type="text" name="age" onChange={ageHandle} className='mainInput' />{ageErr?<span className='ErroeMessage'>Age is Invalid</span>:""}
+                        <button type="submit" className='mainButton'>submit</button>
                     </form>
                 </div>
             </div>
@@ -101,4 +125,4 @@ const MainForm = () => {
     );
 }
 
-export default MainForm;
+export default MainForm; 
